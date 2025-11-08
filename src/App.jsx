05f66a994e2088fecef09c409c89593384f2a8e6
sohidul8305 <si616132@gmail.com>
2025-11-08 23:14:31 +0600

    all setup done

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+     
+      <h1 className='text-center'>Assignment 10</h1>
+
+  
+       
+    </>
+  )
+}
+
+export default App
